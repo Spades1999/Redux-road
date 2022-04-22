b92cf32f1113529c7d18355ef90485c98e4b0886
Lavinia <58559724+Spades1999@users.noreply.github.com>
2022-04-22 10:36:17 +0200

    Add files via upload

---
+++ redux-road.js
@@ -0,0 +1,94 @@
+const initialWagonState = {
+    supplies: 100,
+    distance: 0,
+    days: 0,
+    cash: 200
+};
+  
+const wagonReducer = (state = initialWagonState, action) => {
+  
+    switch(action.type) {
+  
+        case 'gather': {
+            return {
+                ...state,
+                supplies: state.supplies + 15,
+                days: state.days + 1
+            }
+        }
+  
+        case 'travel': {
+            if(state.supplies - ( 20 * action.payload) <= 0) {
+            return state;
+            } else {
+                return {
+                    ...state,
+                    supplies: state.supplies - (20 * action.payload),
+                    distance: state.distance + (10 * action.payload),
+                    days: state.days + action.payload
+                }
+            }
+        }
+  
+        case 'tippedWagon': {
+            return {
+                ...state,
+                supplies: state.supplies - 30,
+                days: state.days + 1
+            }
+        }
+  
+        case 'sell': {
+            return {
+                ...state,
+                supplies: state.supplies - 20,
+                cash: state.cash + 5
+            }
+        }
+    
+        case 'buy': {
+            return {
+                ...state,
+                supplies: state.supplies + 20,
+                cash: state.cash - 15         
+            }
+        }
+  
+        case 'theft': {
+            return {
+                ...state,
+                cash: state.cash / 2
+            }
+        }
+    
+        default: {
+            return state;
+        }
+    }  
+};
+  
+//INITIAL STATE
+let wagon = wagonReducer(undefined, {});
+console.log(wagon);
+  
+//FIRST DAY
+console.log(wagon = wagonReducer(wagon, {type:'travel',payload: 1}));
+//SECOND DAY
+console.log(wagon = wagonReducer(wagon, {type:'gather'}));
+//THIRD DAY
+console.log(wagon = wagonReducer(wagon, {type:'tippedWagon'}));
+//FORTH DAY
+console.log(wagon = wagonReducer(wagon, {type:'travel',payload: 3}));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
